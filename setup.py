ab8ec1bb9e100cdc56c9d3411dcce305a2be7f64
bgdnlp <tropolib-github@neant.ro>
2020-04-27 22:49:39 +0300

    first commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="tropolib",
+    version="0.1",
+    author="bgdnlp",
+    author_email="tropolib-github@neant.ro",
+    url="https://github.com/bgdnlp/tropolib",
+    download_url="https://github.com/bgdnlp/tropolib/archive/master.zip",
+    packages=["tropolib"],
+    package_dir={"tropolib": "src"},
+)
